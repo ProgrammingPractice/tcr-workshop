48fad02432fbb7b7286e6065e8ac4f734cd9fc8c
Florin Oltean <florin.oltean@metoda.com>
2021-01-31 11:53:06 +0100

    working

--- tcr-ruby/spec/fibonacci_spec.rb
+++ tcr-ruby/spec/fibonacci_spec.rb
@@ -1,9 +1,10 @@
 
-def fib()
+def fib(n)
+  0
 end
 
 describe "Calculate the Fibonacci of a number" do
     it "returns 0 for fib(0)" do
-        expect(fib()).to eq(nil)
+        expect(fib(0)).to eq(0)
     end
 end
